43b6684995f1e370529a6d234004b4668fd0c9e9
Shane Gibson <shane.a.gibson@gmail.com>
2015-05-09 15:02:52 +0100

    Added test for multiple items

--- src/test/java/uk/co/epsilontechnologies/shoppingcart/CartTest.java
+++ src/test/java/uk/co/epsilontechnologies/shoppingcart/CartTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.assertEquals;
+import static uk.co.epsilontechnologies.shoppingcart.Product.APPLE;
+import static uk.co.epsilontechnologies.shoppingcart.Product.ORANGE;
 
 public class CartTest {
 
@@ -15,12 +17,17 @@ public class CartTest {
 
     @Test
     public void shouldCalculateTotalCostOfSixtyPenceForOneApple() {
-        assertTotalCartCost("0.60", Product.APPLE);
+        assertTotalCartCost("0.60", APPLE);
     }
 
     @Test
     public void shouldCalculateTotalCostOfTwentyFivePenceForOneOrange() {
-        assertTotalCartCost("0.25", Product.ORANGE);
+        assertTotalCartCost("0.25", ORANGE);
+    }
+
+    @Test
+    public void shouldCalculateTotalCostOfTwoPoundsAndFivePenceForThreeApplesAndOneOrange() {
+        assertTotalCartCost("2.05", APPLE, APPLE, ORANGE, APPLE);
     }
 
     private void assertTotalCartCost(final String expectedTotalCost, final Product... products) {
