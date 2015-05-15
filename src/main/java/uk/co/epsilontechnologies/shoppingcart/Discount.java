a4fbde6128a2784a6a99e4c4b71b9a0feb517f79
Shane Gibson <shane.a.gibson@gmail.com>
2015-05-15 14:12:21 +0100

    Renamed product sorting comparator

--- src/main/java/uk/co/epsilontechnologies/shoppingcart/Discount.java
+++ src/main/java/uk/co/epsilontechnologies/shoppingcart/Discount.java
@@ -1,13 +1,13 @@
 package uk.co.epsilontechnologies.shoppingcart;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 import static java.math.BigDecimal.ZERO;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
+
 public class Discount {
 
     private final int buyCount;
@@ -31,14 +31,14 @@ public class Discount {
 
         return filteredProducts
                 .stream()
-                .sorted(sortLowestToHighest().reversed())
+                .sorted(sortHighestToLowest())
                 .map(product -> product.getUnitPrice())
                 .skip(numberOfPaidProducts)
                 .reduce(ZERO, BigDecimal::add);
     }
 
-    private static Comparator<? super Product> sortLowestToHighest() {
-        return (product1, product2) -> product1.getUnitPrice().compareTo(product2.getUnitPrice());
+    private Comparator<? super Product> sortHighestToLowest() {
+        return (product1, product2) -> - product1.getUnitPrice().compareTo(product2.getUnitPrice());
     }
 
 }
